3c6e527b00fef1b5d10b4d0145cbda036a645a5b
dremofly <dremofly@gmail.com>
2021-07-20 16:00:01 +0800

    完成account list界面

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Dashboard from "../views/Dashboard.vue";
 import Icons from "../views/Icons.vue";
 import Maps from "../views/Maps.vue";
 import Profile from "../views/UserProfile.vue";
+import Account from "../views/AccountsList";
 import Tables from "../views/Tables.vue";
 
 import Login from "../views/Login.vue";
@@ -55,6 +56,12 @@ const routes = [
         name: "profile",
         components: { default: Profile },
       },
+      // TODO: 需要加到主页的路径中，通过api可以获得account的总数
+      {
+        path: "/account",
+        name: "Accounts",
+        components: { default: Account },
+      },
       {
         path: "/tables",
         name: "tables",
@@ -62,7 +69,6 @@ const routes = [
       },
     ],
   },
-
 ];
 
 const router = createRouter({
---
+++ src/views/Tables/AccountsTable.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="card shadow" :class="type === 'dark' ? 'bg-default' : ''">
+    <div
+      class="card-header border-0"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    >
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0" :class="type === 'dark' ? 'text-white' : ''">
+            {{ title }}
+          </h3>
+        </div>
+        <div class="col text-right">
+          <base-button type="primary" size="sm">See all</base-button>
+        </div>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <base-table
+        class="table align-items-center table-flush"
+        :class="type === 'dark' ? 'table-dark' : ''"
+        :thead-classes="type === 'dark' ? 'thead-dark' : 'thead-light'"
+        tbody-classes="list"
+        :data="tableData"
+      >
+        <template v-slot:columns>
+          <th>ID.</th>
+          <th>Address</th>
+          <th>Created Time</th>
+          <th></th>
+        </template>
+
+        <template v-slot:default="row">
+          <td>
+            {{ row.item._id }}
+          </td>
+          <td class="address">
+            {{ row.item.address }}
+          </td>
+          <td class="firstusetime">
+            {{ row.item.firstusetime }}
+          </td>
+        </template>
+      </base-table>
+    </div>
+
+    <div
+      class="card-footer d-flex justify-content-end"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    >
+      <base-pagination
+        :total="this.totalAccount"
+        :value="this.pagination"
+        v-on:input="pageChange($event)"
+      ></base-pagination>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "accounts-table",
+  props: {
+    type: {
+      type: String,
+    },
+    title: String,
+  },
+  data() {
+    return {
+      tableData: [],
+      totalAccount: 100,
+      pagination: 1,
+      resultsPerPage: 10,
+    };
+  },
+  created() {
+    this.getAccoutsList(0);
+  },
+  methods: {
+    pageChange(pageNumber) {
+      this.pagination = pageNumber;
+      const skip = (pageNumber - 1) * this.resultsPerPage;
+      this.getAccoutsList(skip);
+    },
+    getAccoutsList(skip) {
+      axios({
+        method: "post",
+        url: "/api",
+        data: {
+          jsonrpc: "2.0",
+          method: "GetAccountsList",
+          params: { Limit: this.resultsPerPage, Skip: skip },
+          id: 1,
+        },
+        headers: {
+          "Content-Type": "application/json",
+          withCredentials: "true",
+          crossDomain: "true",
+        },
+      })
+        .then((res) => {
+          console.log(res["data"]["result"]["totalCount"]);
+          this.tableData = res["data"]["result"]["result"];
+          this.totalAccount = res["data"]["result"]["totalCount"];
+        })
+        .catch((err) => {
+          console.log("Error", err);
+        });
+    },
+  },
+};
+</script>
+<style></style>
