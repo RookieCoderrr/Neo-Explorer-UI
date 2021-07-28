2a1e64ef66775817038c68c3605a541d6615ca30
WSbaikaishui <46080358+WSbaikaishui@users.noreply.github.com>
2021-07-28 14:42:32 +0800

    version1.0

---
+++ src/components/CollapseTable.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "CollapseTable"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Blocks.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Blocks"
+    }
+</script>
+
+<style scoped>
+
+</style>
