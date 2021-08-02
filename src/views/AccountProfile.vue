ddd8982409d474de4ded74228a693fdc12385104
dremofly <dremofly@gmail.com>
2021-08-02 13:32:20 +0800

    注释contract deployment部分

--- src/views/AccountProfile.vue
+++ src/views/AccountProfile.vue
@@ -67,12 +67,12 @@
                 <div class="col-3">
                   <h3>{{ this.createdTime }}</h3>
                 </div>
-                <div class="col-2">
+                <!--div class="col-2">
                   <div class="text-muted">Contract deployment:</div>
                 </div>
                 <div class="col-3">
                   <h3>{{}}</h3>
-                </div>
+                </div-->
               </div>
               <tabs fill class="flex-column flex-md-row">
                 <tab-pane icon="ni ni-money-coins" title="Token Balance">
@@ -111,7 +111,7 @@
                     </div>
                   </div>
                 </tab-pane>
-                <tab-pane icon="ni ni-collection" title="Contracts"> </tab-pane>
+                <!--tab-pane icon="ni ni-collection" title="Contracts"> </tab-pane-->
               </tabs>
             </div>
           </div>
