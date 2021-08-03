d6b4dca13e1f330ff0d787eb646e8fa2641ca9e4
Zifan Wang <zifanwangsteven@gmail.com>
2021-08-03 13:59:04 +0800

    changed format

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -8,10 +8,10 @@
       <div class="h2 font-weight-bold mb-0">
         Data formats we currently support:
       </div>
-      <p class="h3 font-weight-bold mb-0">Block Hash (UInt256)</p>
-      <p class="h3 font-weight-bold mb-0">Transaction ID (UInt256)</p>
-      <p class="h3 font-weight-bold mb-0">Account Script Hash (UInt160), Account Address (WIF)</p>
-      <p class="h3 font-weight-bold mb-0">Token / Contract Script Hash (UInt160)</p>
+      <p class="text-muted font-weight-bold  mb-0">Block Hash (UInt256)</p>
+      <p class="text-muted font-weight-bold  mb-0">Transaction ID (UInt256)</p>
+      <p class="text-muted font-weight-bold  mb-0">Account Script Hash (UInt160), Account Address (WIF)</p>
+      <p class="text-muted font-weight-bold  mb-0">Token / Contract Script Hash (UInt160)</p>
       <div class="row mt-3"></div>
       <div>
         <base-button type="primary"  @click="toHomePage()">Back Home</base-button>
