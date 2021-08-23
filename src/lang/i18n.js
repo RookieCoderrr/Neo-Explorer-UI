ba48f8ee2ccd8f1aca634361368c69f429853dcb
dremofly <dremofly@gmail.com>
2021-08-23 15:24:30 +0800

    il8n改名为i18n

--- src/lang/i18n.js
+++ src/lang/i18n.js
@@ -1,10 +1,10 @@
 import { createI18n } from "vue-i18n/index"; //引入vue . i18n组件
 import messages from "./index.js";
 //注册i8n实例并引入语言文件
-const il8n = createI18n({
+const i18n = createI18n({
   locale: localStorage.lang || "en",
   messages,
 });
 
 //引入语言文件
-export default il8n; //暴露出去，在main. js中引入挂载
+export default i18n; //暴露出去，在main. js中引入挂载
--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ArgonDashboard from "./plugins/argon-dashboard";
-import il8n from "./lang/il8n.js";
+import i18n from "./lang/i18n.js";
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import './assets/css/global.css'
 
 const appInstance = createApp(App);
 appInstance.use(router);
-appInstance.use(il8n);
+appInstance.use(i18n);
 appInstance.use(ElementPlus)
 appInstance.use(ArgonDashboard);
 appInstance.mount("#app");
