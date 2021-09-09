962d349559e5d6f62b049ef69105a7fe1062ac52
RookieCoderrr <zilie.imtatlantique@gmail.com>
2021-09-09 11:28:10 +0800

    delete useless files and add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,59 @@
+// const LOCAL_API = 'http://192.168.1.89:1926';
+// const VINCENT_API = 'https://testneofura.ngd.network:444';
+// const proxy = {};
+// const proxyInfo = (path) => {
+//   return {
+//     target: path,
+//     changeOrigin: true,
+//     // pathRewrite: {
+//     //   [`^/${path}`]: `/`
+//     // }
+//   }
+// };
+// function setProxy(localApiPath){
+//   proxy[`/api`] = proxyInfo(localApiPath)
+//   console.log(proxy)
+// }
+// setProxy(VINCENT_API)
+
+const webpack = require("webpack");
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        // target: "http://127.0.0.1:1926",
+        target: "https://testneofura.ngd.network:444",
+        // target: "http://106.14.204.151:1926",
+        // target: "http://192.168.1.89:1926"
+      },
+    },
+    // proxy
+  },
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    externals:{
+      vue: "Vue",
+      // axios: "axios",
+      "element-plus": "ElementPlus"
+    },
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 6,
+      }),
+    ],
+  },
+  pwa: {
+    name: "Vue Argon Design",
+    themeColor: "#172b4d",
+    msTileColor: "#172b4d",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#172b4d",
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== "production",
+  },
+};
+
