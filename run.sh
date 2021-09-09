3e713d2f23a13e9f28445d83094f6db2de3f56de
RookieCoderrr <zilie.imtatlantique@gmail.com>
2021-09-09 11:07:22 +0800

    add dist file

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker stop vueApp
-docker container rm vueApp
-docker rmi neo-explorer
-docker image build . -t neo-explorer
-docker run -itd --name vueApp --link service_http_test --net network -p 8080:80 neo-explorer
+docker stop vueAppTestNet
+docker container rm vueAppTestNet
+docker rmi neo-explorer-testnet
+docker image build . -t neo-explorer-testnet
+docker run -itd --name vueAppTestNet -p 8080:80 neo-explorer-testnet
